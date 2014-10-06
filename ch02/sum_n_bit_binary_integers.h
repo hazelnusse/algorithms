82aea4777c0a1442283590c1d582c2f20c3626fd
Dale Lukas Peterson <hazelnusse@gmail.com>
2014-10-06 15:46:46 -0700

    Exercise 2.1-4

---
+++ ch02/sum_n_bit_binary_integers.h
@@ -0,0 +1,47 @@
+#ifndef SUM_N_BIT_BINARY_INTEGERS_H
+#define SUM_N_BIT_BINARY_INTEGERS_H
+
+/** Formal problem statement:
+ * Input: Two sequences of N bits, stored in two n-element arrays A and B
+ * Output: One sequence of N+1 bits, stored in one (n+1)-element array C which
+ * contains the sum of A & B.
+ */
+
+template <std::size_t N>
+std::bitset<N + 1> sum(const std::bitset<N>& a, const std::bitset<N>& b) {
+
+    std::bitset<N + 1> c;
+    bool carry = false;
+    for (std::size_t i = 0; i != N; ++i ) {
+        if (a[i] && b[i]) {
+            if (carry) {
+                c[i] = true;
+            } else {
+                c[i] = false;
+            }
+            carry = true;
+        } else if (a[i] || b[i]) {
+            if (carry) {
+                c[i] = false;
+            } else {
+                c[i] = true;
+            }
+        } else {
+            if (carry) {
+                c[i] = true;
+            } else {
+                c[i] = false;
+            }
+            carry = false;
+        }
+    }
+    if (carry) {
+        c[N] = true;
+    } else {
+        c[N] = false;
+    }
+    return c;
+}
+
+#endif
+
---
+++ ch02/test_sum_n_bit_binary_integers.cc
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <bitset>
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+#include "sum_n_bit_binary_integers.h"
+
+template <class Container>
+void print(const Container& c) {
+    std::copy(c.begin(), c.end(),
+            std::ostream_iterator<typename Container::value_type>(std::cout, ", "));
+    std::cout << "\n";
+}
+
+class Test_sum: public ::testing::Test {
+public:
+    std::bitset<5> a;
+    std::bitset<5> b;
+    Test_sum() : a{}, b{} {}
+};
+
+TEST_F(Test_sum, Sum) {
+    a = decltype(a){"10111"};
+    b = decltype(b){"10111"};
+    ASSERT_EQ(std::bitset<6>{"101110"}, sum(a, b));
+    a = decltype(a){"10101"};
+    b = decltype(b){"01010"};
+    ASSERT_EQ(std::bitset<6>{"011111"}, sum(a, b));
+    a = decltype(a){"00000"};
+    b = decltype(b){"00000"};
+    ASSERT_EQ(std::bitset<6>{"000000"}, sum(a, b));
+}
+
