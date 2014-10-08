67b647b666cce7bd7efe8e0a80c96deb1a6bb419
Dale Lukas Peterson <hazelnusse@gmail.com>
2014-10-08 16:23:41 -0700

    Implement binary search

--- ch02/binary_search.h
+++ ch02/binary_search.h
@@ -1,29 +1,48 @@
 #ifndef BINARY_SEARCH_H
 #define BINARY_SEARCH_H
 
-#include <iterator>
+#include <functional>       // for std::less
+#include <iterator>         // for std::distance, std::advance
 
+namespace alg {
+
+/**
+ * \brief Find iterator to value in range [begin, end), returning
+ * end if not found.
+ *
+ * It is assumed that [begin, end) is sorted with respect to comp.
+ */
 template <class Iterator,
-          typename Compare = std::less<typename std::iterator_traits<Iterator>::value_type>>
+          typename U,
+          typename Compare = std::less<U>>
 Iterator binary_search(Iterator begin,
                        Iterator end,
-                       const typename std::iterator_traits<Iterator>::reference value,
+                       U const& value,
                        Compare comp = Compare())
 {
-    Iterator mid = begin;
-    std::advance(mid, std::distance(begin, end) / 2);
-    if (value == *mid) {                // value is at midpoint
-        return mid;
-    } else if (comp(value, *mid)) {     // value < *mid
-        auto result = binary_search(begin, mid, value, comp);
-        if (result == mid) { // didn't find value in [begin, mid)
-            return end;
+    auto N = std::distance(begin, end);
+    if (N > 1) {
+        Iterator mid = begin;
+        std::advance(mid, std::distance(begin, end) / 2);
+        if (value == *mid) {                // value is at midpoint
+            return mid;
+        } else if (comp(value, *mid)) {     // value < *mid
+            Iterator result = alg::binary_search(begin, mid, value, comp);
+            if (result == mid) { // didn't find value in [begin, mid)
+                return end;
+            }
+            return result;
+        } else {                            // value > *mid
+            return alg::binary_search(mid, end, value, comp);
         }
-        return result;
-    } else {                            // value > *mid
-        return binary_search(mid, end, value, comp);
+    } else if (N == 1 && value == *begin) {
+        return begin;
+    } else {
+        return end;
     }
 }
+
+} // namespace alg
 
 #endif
 
--- ch02/test_binary_search.cc
+++ ch02/test_binary_search.cc
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <array>
 #include <iostream>
 
 #include <gtest/gtest.h>
@@ -11,7 +12,7 @@ class Test_binary_search: public ::Test_random_vector {};
 TEST_F(Test_binary_search, FindsElementsInVector) {
     std::sort(v.begin(), v.end());
     for (std::size_t i = 0; i != v.size(); ++i) {
-        auto result = binary_search(begin(v), end(v), v[i]);
+        auto result = alg::binary_search(begin(v), end(v), v[i]);
         if (result == end(v)) {
             ASSERT_FALSE(std::binary_search(begin(v), end(v), v[i])); // STL binary search
         } else {
@@ -25,7 +26,7 @@ TEST_F(Test_binary_search, FindsElementsInReverseVector) {
     using comp = std::greater<decltype(v)::value_type>;
     std::sort(v.begin(), v.end(), comp());
     for (std::size_t i = 0; i != v.size(); ++i) {
-        auto result = binary_search(begin(v), end(v), v[i], comp());
+        auto result = alg::binary_search(begin(v), end(v), v[i], comp());
         if (result == end(v)) {
             ASSERT_FALSE(std::binary_search(begin(v), end(v), v[i], comp())); // STL binary search
         } else {
@@ -33,5 +34,12 @@ TEST_F(Test_binary_search, FindsElementsInReverseVector) {
             ASSERT_EQ(v[i], *result);   // values compare equal
         }
     }
+    ASSERT_EQ(end(v), alg::binary_search(begin(v), end(v), v[0]+1, comp()));
+}
+
+TEST_F(Test_binary_search, FindsElementInLengthOneArray) {
+    std::array<int, 1> ar{1};
+    ASSERT_EQ(begin(ar), alg::binary_search(begin(ar), end(ar), ar[0]));
+    ASSERT_EQ(end(ar), alg::binary_search(begin(ar), end(ar), 5));
 }
 
